8cd954f7d4d14385355283b4c34d87e96d86616b
minumadhvani <127842573+minumadhvani@users.noreply.github.com>
2024-09-23 16:58:06 -0600

    updated button labelling aesthetics

--- cart.jsx
+++ cart.jsx
@@ -133,7 +133,7 @@ const Products = (props) => {
       <li key={index}>
         <Image src={photos[index % 4]} width={70} roundedCircle></Image>
         <Button variant="primary" size="large">
-          {item.name}:${item.cost}:{item.instock}
+          {item.name}: ${item.cost} : Stock: {item.instock}
         </Button>
         <input name={item.name} type="submit" onClick={addToCart}></input>
       </li>
